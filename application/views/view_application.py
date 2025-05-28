f58dab35877efce2fe72a2e50656d8794c489a89
Aslon <arslon@gmail.com>
2025-05-28 16:26:42 +0500

    Commit

--- application/views/view_application.py
+++ application/views/view_application.py
@@ -38,20 +38,19 @@ class ApplicationCreateView(View):
         valid_window = YearlyApplicationWindow.objects.filter(year=date.year).first()
         if valid_window:
             if student.gpa < valid_window.min_gpa:
-                messages.info(request, "Sizning GPA talablarga javob bermaydi.")
+                messages.info(request, f"Sizning GPA talablarga javob bermaydi. \n Minimal GPA: {valid_window.min_gpa}\n Sizning GPA: {student.gpa}\n Agar GPA ma'lumotlaringiz yangilanmagan bo'lsa, iltimos,Registrator offisiga murojaat qiling.")
                 return redirect('main:index') 
             
-
             if date < valid_window.start_date or date > valid_window.end_date:
-                messages.info(request, "Ariza topshirish muddati tugagan.")
+                messages.info(request, "Ariza topshirish yopiq.")
                 return redirect('main:index')
             
             if not get_course(student.level, valid_window.course):
-                messages.info(request, "Sizning kursingiz ushbu ariza oynasiga mos kelmaydi.")
+                messages.info(request, "Sizning kursingiz bu yil ariza topshirish uchun mos emas.")
                 return redirect('main:index')
             
         if Application.objects.filter(student=student, created_at__year=date.year).exists():
-            messages.info(request, "Siz bu yil ariza topshirgansiz allaqachon ariza topshirgansiz.")
+            messages.info(request, "Siz allaqachon bu yil ariza topshirgansiz.")
             return redirect('main:index') 
         
         new_application = Application.objects.create(student=student, application_status=ApplicationStatus.NEW)
