d1f363c39488dedccd950ef71d6a824cb75609a6
Aslon <arslon@gmail.com>
2025-05-29 17:44:56 +0500

    Commit

--- main/views/view_home.py
+++ main/views/view_home.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from application.models import Application
 
 def index(request):
-    return render(request, 'result/index.html')
+    data = {
+        'students_count': 1980,
+        'applications_count': Application.objects.all().count(),
+        'approved_applications': Application.objects.filter(application_status='approved').count(),
+        'rejected_applications': Application.objects.filter(application_status='rejected').count()
+    }
+    return render(request, 'result/index.html', context=data)
